dc8b1dc123c3cff94c1aac66c20634e6d75d89c0
Muhammad Fazle <f4zlerabbani@gmail.com>
2024-08-01 21:09:13 +0700

    finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-/* Ini JavaScript */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (name == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+    if (email == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address");
+        return false;
+    }
+
+    return true;
+}
